2d370d57117638186697d079288dfa441eaadf92
Mustafa Neto <mustafaneto26@gmail.com>
2023-03-09 14:09:22 -0300

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,3 +81,16 @@ modalCloses.forEach((modalClose) => {
     })
   })
 })
+
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
